2ddef55d324d9e7355b62799c5968e5ffd109690
srivardhan113 <105148963+srivardhan113@users.noreply.github.com>
2023-12-22 16:29:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./WordCounter";
+import WordCounter from "./WordCounter";
+function App() {
+  return (
+    <div className="App">
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
